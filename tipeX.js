f4f1c5705b9062e8b36d46ed93e91bb5535e730d
GLEB687 <glebzlun@mail.ru>
2026-01-13 19:45:27 +0300

    1

---
+++ tipeX.js
@@ -0,0 +1,44 @@
+// строка 
+let text = "это строка"
+
+console.log("text", "тип", typeof text, "значение", text)
+
+// число 
+let age = 24
+console.log("age", "тип", typeof age, "значение", age)
+
+// буливый тип 
+let itisTrue = true;
+console.log("itisFalse", "тип", typeof itisTrue, "значение", itisTrue)
+
+let itisFalse = false;
+console.log("itisFalse", "тип", typeof itisFalse, "значение", itisFalse)
+
+
+// огромное число 
+let countofstar = 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999990009999999999999999999999999999999999999999999999999999999999999999999999999999999999999n
+console.log("countofstar", "тип", typeof countofstar, "значение",countofstar)
+
+//
+
+let unknowm
+console.log("unknowm", "тип", typeof unknowm,
+ "значение",unknowm)
+
+
+ let empty = null
+console.log("empty", "тип", typeof empty,
+ "значение",empty)
+
+ let human = {};
+ console.log("human", "тип", typeof human,
+  "значение",human)
+
+
+
+
+
+
+
+
+
---
+++ x.js
@@ -0,0 +1,28 @@
+
+// обявление переменной 
+let text
+text = "hello"
+
+console.log(text)
+
+text = "hi"
+console.log(text)
+
+let msg = "8"
+
+let a1, a2, a3
+
+a1 =  "1"
+a2 =  "2"
+a3 =  "3"
+
+const pi = 3.1415926;
+console.log(pi)
+
+var age = 20;
+console.log(age)
+
+//строчки, числа, буливый тип, объекты
+
+let text2 = "15"
+console.log(text2)
