dee9a00eb66d339353a4b407dffae55cf88d272e
GLEB687 <glebzlun@mail.ru>
2026-01-20 19:03:10 +0300

    2

---
+++ opera.js
@@ -0,0 +1,16 @@
+// операторы в js
+// уныарные апператоры
+let pozitivN = +4
+let negativN = -4
+
+let caunt = "45";
+let cauntAS = +caunt;
+console.log(cauntAS, typeof cauntAS);
+
+let is = "yes";
+let cauntis = !-is;
+console.log(cauntis, typeof cauntis);
+
+
+
+
---
+++ t.js
@@ -0,0 +1,37 @@
+
+
+// преобразование к строке
+
+let tostfromnymber = String(1);
+console.log(tostfromnymber, typeof tostfromnymber);
+
+let obj = {};
+let = tostfromO = String(obj)
+console.log(tostfromO, typeof tostfromO);
+
+// преобразование к числу 
+console.log(Number(""));
+console.log(Number("0"));
+console.log(Number("124.9"));
+console.log(Number("one"));
+console.log(Number("My home"));
+console.log(Number("e769"));
+console.log(Number("102,5"));
+console.log(Number("             8         ss"));
+console.log(Number(true));
+console.log(Number(null));
+console.log(Number({}));
+console.log("----------------------------------------------------------------");
+// преобразование к Булу
+console.log(Boolean({}));
+console.log(Boolean(""));
+console.log(Boolean("1"));
+console.log(Boolean("0"));
+console.log(Boolean("true"));
+console.log(Boolean("folse"));
+console.log(Boolean("home"));
+console.log(Boolean("                     "));
+console.log(Boolean("19"));
+console.log(Boolean("-86"));
+console.log(Boolean(0));
+console.log(Boolean({}));
